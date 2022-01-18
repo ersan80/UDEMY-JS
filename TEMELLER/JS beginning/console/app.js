111630fc4e14cb97822e6e4a0838e08d0c9c4ed4
ersan80 <ersan.kuleli80@gmail.com>
2022-01-18 16:18:36 +0300

    udemyjs

---
+++ TEMELLER/JS beginning/console/app.js
@@ -0,0 +1,117 @@
+console.log("nerhaba")
+
+
+//Primitive
+
+var a = 10 // number veri tipi
+
+var b = 3.14
+
+console.log(a,b)
+
+// string
+
+var name = "js"
+console.log(name)
+
+console.log(typeof name)
+
+//Boolean
+
+var a = true
+
+console.log(typeof a)
+
+// NULL
+
+var a = null
+console.log(typeof a)
+
+//
+
+var a 
+a = 20
+
+console.log(a)
+console.log(typeof a)
+
+
+
+
+
+
+
+//Reference veri Tipi
+
+
+var numbers = [1,2,3,4,5]
+
+console.log(numbers)
+console.log(typeof numbers)
+console.log(numbers[0])
+
+var person = {
+    name: "cihan",age: "38"
+    
+
+}
+
+console.log(person)
+
+var date = new Date()
+
+console.log(date)
+console.log(typeof date)
+
+var merhaba = function() {
+    console.log("merhaba")
+
+
+}
+
+console.log(merhaba)
+
+var a= 10
+
+var b = a
+
+console.log(a,b)
+
+a = 20
+
+console.log(a,b)
+
+var a = [1,2,3]
+
+var b = a 
+
+console.log(b)
+
+a.push(985)
+
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
