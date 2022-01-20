f8b02b4b498d7c0d6b9da79e68b600011ba66056
ersan80 <ersan.kuleli80@gmail.com>
2022-01-20 20:50:51 +0300

    second commit

---
+++ TEMELLER/JS beginning/console/var,let,const/operatörler ve math objesi/stringler/template literal/ARRAYLER/objeler/date obje/koşul durumları/fonksiyonlar/Döngüler/Window obje/Scop/app13.js
@@ -0,0 +1,37 @@
+//Scope Kavramı
+
+
+
+// Global Scop
+
+///function a(){
+    //Function scobe
+//}
+
+//if (){
+    //block scobe
+//}
+
+
+//Global scobe
+
+var value1 = 10;
+let value2 = 20;
+const value3 = 30;
+
+
+
+
+
+function func(){
+    var value1 = 110;
+    let value2 = 320;
+    const value3 = 530;
+
+    console.log(value1,value2,value3)
+}
+
+
+
+
+console.log(value1,value2,value3);
---
+++ TEMELLER/JS beginning/console/var,let,const/operatörler ve math objesi/stringler/template literal/ARRAYLER/objeler/date obje/koşul durumları/fonksiyonlar/Döngüler/Window obje/app12.js
@@ -0,0 +1,48 @@
+
+
+////alert("merhaba")
+
+//const cevap = confirm("eminmisin");
+/* console.log(cevap); */
+//confirm
+
+/* if(confirm("eminmisiniz")) {
+    console.log("silin")
+}
+
+else{
+    console.log("hayııır")
+} */
+
+/* const cevap = prompt("2+2= ?")
+
+if(cevap == "4"){
+    console.log("Doğru Bildiniz")
+}
+
+else {
+    console.log("deyyuz")
+} */
+/* if(confirm("sayfa yenilenmsin mi")){
+    window.location.reload();
+}
+
+else{
+    console.log("by")
+} */
+
+let value;
+
+value = window;
+value = window.location;
+value =  window.location.hostname
+value = window.location.href
+
+value = window.outerHeight
+value = window.outerWidth
+value = window.scrollY
+
+
+console.log(value)
+
+
