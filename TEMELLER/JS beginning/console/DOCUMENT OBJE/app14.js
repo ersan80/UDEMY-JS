79e5830eb2ffb35e082f6e23adbb07bb71be3bd5
ersan80 <ersan.kuleli80@gmail.com>
2022-01-24 14:27:45 +0300

    com

---
+++ TEMELLER/JS beginning/console/DOCUMENT OBJE/app14.js
@@ -0,0 +1,27 @@
+/* const element = document.all;
+
+for(let i = 0; i<element.length;i++){
+    console.log(element[i]);
+}
+ */
+
+let value;
+
+
+value = document.scripts;
+
+value = document.scripts.length;
+
+value = document.links[0];
+
+value = document.links;
+
+value = document.querySelector("li")// tek eleman seçerken query merodu kullanılır
+
+value.forEach(function(i) {
+
+    console.log(i);
+    
+});
+
+console.log(value);
